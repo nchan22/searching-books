a271926e258d3f62c6db740bc18a333d146a2101
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 16:52:54 -0400

    Add_user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,24 @@
 import gql from "graphql-tag";
+
+// add a mutation that adds a user that when entering a username, email and password, it needs to be a string. based on same token and user details
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+          bookId
+          title
+          description
+          authors
+          image
+          link
+        }
+      }
+    }
+  }
+`;
