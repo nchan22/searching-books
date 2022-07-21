2fe199cb0164b221e600731bee52111c7d3a1187
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 17:19:29 -0400

    Added book definition

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,3 +1,12 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+`;
