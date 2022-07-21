f6dc67cd69f5d6a485b16bad91ae53da816cda1f
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 17:21:46 -0400

    Added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -15,3 +15,22 @@ const resolvers = {
             throw new AuthenticationError('Not logged in');
         }
     },
+
+    Mutation: {
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
