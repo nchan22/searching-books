eaa6c7f27b89d4bc56cc38de73ff17ea5d24cc2a
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 17:22:29 -0400

    Added saveBook to mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -41,3 +41,15 @@ const resolvers = {
 
             return { token, user };
         },
+
+        saveBook: async (parent, { input }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You need to be logged in!')
+        },
