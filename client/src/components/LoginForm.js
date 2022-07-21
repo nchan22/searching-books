6cc3bcdb160e7556cec0f0e3d7412aa53e87ba89
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 17:07:44 -0400

    Update loginForm function

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -6,13 +6,17 @@ import { LOGIN_USER } from "../utils/mutations";
 import { useMutation } from "@apollo/react-hooks";
 
 const LoginForm = () => {
+  const [login, { error }] = useMutation(LOGIN_USER);
   const [userFormData, setUserFormData] = useState({ email: "", password: "" });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+    setUserFormData({
+      ...userFormData,
+      [name]: value,
+    });
   };
 
   const handleFormSubmit = async (event) => {
