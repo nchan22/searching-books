bed248446d212f0fb594836c3ed5471e9ec65249
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 16:57:01 -0400

    Add get user query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,21 @@
 import gql from "graphql-tag";
+
+// add query to get a user
+export const GET_USER = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
