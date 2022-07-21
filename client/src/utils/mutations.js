f7cd2cc35e59ad504b39cc10570d72c6b0ece6f2
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 16:55:51 -0400

    Add save_book function

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-// add a mutation that adds a user that when entering a username, email and password, it needs to be a string. based on same token and user details
+// add a mutation that adds a user that when entering a username, email and password, it must exist. based on same token and user details
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -23,7 +23,7 @@ export const ADD_USER = gql`
   }
 `;
 
-// add login user that requires the email & password to be a string, and when logged in, will have same token and matching user info
+// add login user that requires the email & password must exist, and when logged in, will have same token and matching user info
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -41,6 +41,25 @@ export const LOGIN_USER = gql`
           link
           image
         }
+      }
+    }
+  }
+`;
+
+// add mutation to save a book. the bookInput must exist
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: bookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
       }
     }
   }
