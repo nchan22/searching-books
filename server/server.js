a7f397879e446e9ec3acfb05308231313043df66
nchan22 <98130524+nchan22@users.noreply.github.com>
2022-07-21 17:35:51 -0400

    Update file for for use of apolloServer and express with middleware

--- server/server.js
+++ server/server.js
@@ -20,13 +20,21 @@ server.applyMiddleware({ app });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
